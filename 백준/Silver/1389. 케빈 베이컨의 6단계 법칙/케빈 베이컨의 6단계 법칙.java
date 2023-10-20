643c11552afc311fae6cbd8aae1d1d5cbbaa87a5
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2023-10-20 23:51:04 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int arr[][]; 
+	static boolean visit[];
+
+	static int N;
+	static int result;
+	static int min_count = Integer.MAX_VALUE;
+
+	static class Bacon {
+		int num;
+		int value;
+
+		public Bacon(int num, int value) {
+			this.num = num;
+			this.value = value;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); // 유저의 수
+		int M = Integer.parseInt(st.nextToken()); // 친구 관계의 수
+
+		arr = new int[N + 1][N + 1];
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+
+			arr[y][x] = 1;
+			arr[x][y] = 1;
+		}
+
+		for(int i=1; i<=N; i++) {
+			visit = new boolean[N + 1];
+			BFS(i);
+		}
+
+		System.out.println(result);
+	}
+
+	static void BFS(int start) {
+		Queue<Bacon> q = new LinkedList<>();
+		int count = 0;
+
+       	visit[start] = true;
+		q.offer(new Bacon(start, 0));
+
+		while( !q.isEmpty() ) {
+			Bacon bacon = q.poll();
+			count += bacon.value;
+
+			for(int i=1; i<=N; i++) {
+				int num = arr[bacon.num][i];
+
+				if(num == 1 && visit[i] == false) {
+					visit[i] = true;
+					q.offer(new Bacon(i, bacon.value + 1));
+				}
+			}
+		}
+
+		if(min_count > count) {
+			min_count = count;
+			result = start;
+		}
+
+	}
+
+}
