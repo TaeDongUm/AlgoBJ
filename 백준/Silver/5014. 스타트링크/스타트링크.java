27d8ae7d410d05cce3d3bd9641af36cb63c7ddcb
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2023-12-02 00:43:20 +0900

    [Silver I] Title: 스타트링크, Time: 208 ms, Memory: 57388 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -1,50 +1,53 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
- 
+import java.util.StringTokenizer;
+
 public class Main {
- 
-    public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = br.readLine().split(" ");
- 
-        int F = Integer.parseInt(str[0]);
-        int S = Integer.parseInt(str[1]);
-        int G = Integer.parseInt(str[2]);
-        int U = Integer.parseInt(str[3]);
-        int D = Integer.parseInt(str[4]);
-        int[] arr = new int[F + 1];
-        System.out.println(BFS(F, S, G, U, D, arr));
- 
-    }
- 
-    public static String BFS(int Floor, int start, int end, int up, int down, int[] arr) {
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.add(start);
-        arr[start] = 1;
- 
-        while (!q.isEmpty()) {
-            int current = q.poll();
-            if (current == end) {
-                return String.valueOf(arr[current] - 1);
-            }
-            if (current + up <= Floor) {
-                if (arr[current + up] == 0) {
-                    arr[current + up] = arr[current] + 1;
-                    q.add(current + up);
-                }
- 
-            }
-            if (current - down > 0) {
-                if (arr[current - down] == 0) {
-                    arr[current - down] = arr[current] + 1;
-                    q.add(current - down);
-                }
-            }
- 
-        }
-        return "use the stairs";
-    }
+	static int[] position;
+	static int goal, start, up, down, building, depth;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		building = Integer.parseInt(st.nextToken());
+		position = new int[building];
+		
+		start = Integer.parseInt(st.nextToken());
+		goal = Integer.parseInt(st.nextToken());
+		up = Integer.parseInt(st.nextToken());
+		down = Integer.parseInt(st.nextToken());
+		
+		System.out.println(bfs(position, start, goal));
+		
+	}
+	public static String bfs(int[] position, int start, int goal) {
+		Queue<Integer> q = new LinkedList<>();
+		q.add(start);
+		position[start-1] = 1;
+		
+		while(!q.isEmpty()) {
+			int current = q.poll();
+			if(current == goal) {
+				return String.valueOf(position[current-1]-1);
+			}
+			if(current + up <=building) {
+				if(position[current +up-1] ==0) {
+					position[current + up -1] = position[current-1]+1;
+					q.add(current + up);
+				}
+			}
+			if(current -down >=1) {
+				if(position[current - down-1] ==0) {
+					position[current - down -1] = position[current-1]+1;
+					q.add(current-down);
+				}
+			}
+			
+			
+		}
+		return "use the stairs";
+	}
 }
