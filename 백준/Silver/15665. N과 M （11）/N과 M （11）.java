0ead01950b19fd2f8e3e337fdde656287e5fae04
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2023-12-25 22:27:44 +0900

    [Silver II] Title: N과 M (11), Time: 396 ms, Memory: 34216 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n, m;
+    public static int[] arr, nums;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        nums = new int[n + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int cnt) {
+
+        if (cnt == m) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int before = 0;
+        for (int i = 0; i <= n; i++) {
+            if (before != nums[i]) {
+                arr[cnt] = nums[i];
+                before = nums[i];
+                dfs(cnt + 1);
+            }
+
+        }
+    }
+}
