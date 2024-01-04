e2e4ad0576218da47f0c8ab53cfd605dc62ea105
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-01-04 11:38:05 +0900

    [Silver IV] Title: 수 찾기, Time: 1252 ms, Memory: 52680 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,11 +2,12 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.io.BufferedReader;
 import java.util.Arrays;
+import java.io.IOException;
 
 public class Main{
     static int[] arr;
     static int[] check;
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
