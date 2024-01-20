f408ca19e78fc136bb923abc7e92adf603efbc84
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-01-21 06:42:24 +0900

    [Gold V] Title: 토마토, Time: 636 ms, Memory: 123068 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,83 +1,71 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-
 public class Main {
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static int n, m;
-    static int[][] map;
-    static Queue<int[]> q = new LinkedList<>();
+	static int[][] board;
+	static int[][] visited;
+	static int days, M, N;
+	static int preTomato;
+	static int[] dx = {1,-1,0,0};
+	static int[] dy = {0,0,1,-1};
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		
+		board = new int[N][M];
+		visited = new int[N][M];
+		
+		Queue<int[]> q = new LinkedList<>();
+		for(int i =0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<M;j++) {
+				int tmp = Integer.parseInt(st.nextToken());
+				if(tmp == 0) preTomato++;
+				if(tmp == 1) {
+					q.add(new int[] {i,j});
+					visited[i][j] =1;
+				}
+				board[i][j] = tmp;
+			}
+		}
+		bfs(board, visited, q);
+		if(preTomato !=0) System.out.println(-1);
+		if(preTomato ==0) System.out.println(days);
+		
+		
+	}
+	public static void bfs(int[][] board, int[][] visited, Queue<int[]> q) {
+		if(preTomato ==0) {
+			days=0;
+			return;
+		}
+		while(!q.isEmpty()) {
+			int[] cur = q.poll();
+			int x = cur[0];
+			int y = cur[1];
+			
+			for(int i=0;i<4;i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				
+				if(nx>=0 && nx<N && ny>=0 && ny<M) {
+					if(board[nx][ny]==0 && visited[nx][ny] !=1) {
+						preTomato--;
+						board[nx][ny] = board[x][y] +1;
+						days = Math.max(days, board[nx][ny]-1);
+						visited[nx][ny] = 1;
+						q.add(new int[] {nx, ny});
+					}
+				}
+			}
+		}
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-
-        map = new int[n][m];
-
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 1) {
-                    q.add(new int[]{i, j});
-                }
-            }
-        }
-
-        System.out.println(bfs());
-    }
-
-    private static int bfs() {
-        while (!q.isEmpty()) {
-            int[] t = q.poll();
-            int x = t[0];
-            int y = t[1];
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if (map[nx][ny] == 0) {
-                    map[nx][ny] = map[x][y] + 1;
-                    q.add(new int[]{nx, ny});
-                }
-            }
-        }
-
-        int max = Integer.MIN_VALUE;
-        if (isZero()) {
-            return -1;
-        } else {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (max < map[i][j]) {
-                        max = map[i][j];
-                    }
-                }
-            }
-
-            return max - 1;
-        }
-
-
-    }
-
-    private static boolean isZero() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (map[i][j] == 0)
-                    return true;
-            }
-        }
-        return false;
-    }
 }
