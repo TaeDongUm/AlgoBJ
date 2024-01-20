2300b8a5368e47b57a47fafd9021a25ce4e589a7
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-01-21 02:27:00 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14704 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,60 +1,60 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
-public class Main{
-	static Queue<int[]> q = new LinkedList<>();
-	static int[][] map;
-	static int[][] visited;
-	static int N,M, count=0;
-	static int[] dx = { -1, 1, 0, 0 }; //x방향배열-상하
-	static int[] dy = { 0, 0, -1, 1 }; //y방향배열-좌우
-	
-	public static void main(String[] args) throws IOException{
+
+public class Main {
+	static int[] dx= {1,-1,0,0};
+	static int[] dy= {0,0,1,-1};
+	static int[][] board, visited;
+	static int N,M;
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		map = new int[N][M];
+		
+		board = new int[N][M];
 		visited = new int[N][M];
+		
+		String input="";
 		for(int i =0;i<N;i++) {
-			String line = br.readLine();
+			input = br.readLine();
 			for(int j=0;j<M;j++) {
-				map[i][j] = Integer.parseInt(line.charAt(j)+"");
-				visited[i][j] = map[i][j];
+				board[i][j] = input.charAt(j) - '0';
 			}
 		}
-		path(0,0);
-		System.out.println(count);
+		
+		bfs(board, visited, 0, 0);
+		System.out.println(board[N-1][M-1]);
 		
 	}
-	private static void path( int x, int y) {
-		q.add(new int[] {x,y});
-		visited[x][y] = 2;
+	public static void bfs(int[][] board, int[][] visited, int a, int b) {
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] {a,b});
+		visited[a][b] = 1;
+		
 		while(!q.isEmpty()) {
-			int size = q.size();
-			count++;
-			for(int i=0;i<size;i++) {
-				int[] current = q.poll();
-				if(current[0]==N-1 && current[1]==M-1) return;
-				int nx, ny;
-				for(int j =0;j<4;j++) {
-					nx = current[0] + dx[j];
-					ny = current[1] + dy[j];
-					if((nx>=0 && nx<N) && (ny>=0 && ny<M)) {
-						if(map[nx][ny]==1 && visited[nx][ny]==1) {
-							q.add(new int[] {nx,ny});
-							visited[nx][ny] = 2;
-						}
+			int[] cur = q.poll();
+			int x = cur[0];
+			int y = cur[1];
+			
+			for(int i=0;i<4;i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				
+				if(nx>=0 && nx<N && ny >=0 && ny <M) {
+					if(board[nx][ny] != 0 && visited[nx][ny] ==0) {
+						visited[nx][ny] = 1;
+						board[nx][ny] = board[x][y] +1;
+						q.add(new int[] {nx, ny});
+						
 					}
 				}
 			}
-			
 		}
-		
 	}
+
 }
