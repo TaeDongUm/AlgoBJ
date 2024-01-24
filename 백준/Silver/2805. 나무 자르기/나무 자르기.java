bfa9fea3bf21ad1133a52e3a88c51d6af0fdb378
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-01-25 06:02:13 +0900

    [Silver II] Title: 나무 자르기, Time: 540 ms, Memory: 119360 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -7,35 +7,29 @@ public class Main {
 	
 	static int N,M;
 	static int[] trees;
-	static int maxHeight, answer=Integer.MIN_VALUE;
-	static int minHeight=Integer.MAX_VALUE;
+	static int answer=Integer.MIN_VALUE;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
+		StringTokenizer st1;
+		st1 = new StringTokenizer(br.readLine());
 		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st1.nextToken());
+		M = Integer.parseInt(st1.nextToken());
 		
 		trees = new int[N];
-		st = new StringTokenizer(br.readLine());
+		st1 = new StringTokenizer(br.readLine());
+        int max =0;
 		for(int i=0;i<N;i++) {
-			trees[i] = Integer.parseInt(st.nextToken());
-			
+			trees[i] = Integer.parseInt(st1.nextToken());
+            max = Math.max(trees[i], max);
 		}
-		Arrays.sort(trees);
-		binaryS(trees);
-		System.out.println(answer);
-		
-	}
-	
-	public static void binaryS(int[] trees) {
 		int st = 0;
-		int en = trees[trees.length-1];
+		int en = max;
 		int target = M;
+        int min;
 		while(st <= en) {
 			long sum=0;
-			int min = (st + en)/2;
+			min = (st + en)/2;
 			for(int  i=0;i<N;i++) {
 				if(min <trees[i]) {
 					sum += trees[i] - min;
@@ -44,13 +38,17 @@ public class Main {
 			if(sum >=target) {
 				st = min+1;
 				answer = Math.max(answer, min);
-				if(sum == target) return;
+				if(sum == target) break;
 			}
 			if(sum < target) {
 				en = min-1;
 			}
 			
 		}
+		System.out.println(answer);
 		
 	}
+
+		
+	
 }
