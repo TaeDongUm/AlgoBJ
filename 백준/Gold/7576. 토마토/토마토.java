fb804a5fcffb31958cd2b5508aa1b581abaf3ee1
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-03-01 17:30:10 +0900

    [Gold V] Title: 토마토, Time: 632 ms, Memory: 120600 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -5,63 +5,59 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] board;
-	static int[][] visited;
-	static int days, M, N;
-	static int preTomato;
-	static int[] dx = {1,-1,0,0};
-	static int[] dy = {0,0,1,-1};
+	static int M, N, count, preTomato;
+	static int[][] map;
+	static int[] dx = {0,0,1,-1};
+	static int[] dy = {1,-1,0,0};
+	static Queue<int[]> q = new LinkedList<>();
 	
 	public static void main(String[] args) throws Exception{
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		st = new StringTokenizer(br.readLine());
 		M = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+		count = 0;
+		preTomato =0;
 		
-		board = new int[N][M];
-		visited = new int[N][M];
-		
-		Queue<int[]> q = new LinkedList<>();
-		for(int i =0;i<N;i++) {
+		for(int i = 0;i<N;i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0;j<M;j++) {
-				int tmp = Integer.parseInt(st.nextToken());
-				if(tmp == 0) preTomato++;
-				if(tmp == 1) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if(map[i][j] == 1) {
 					q.add(new int[] {i,j});
-					visited[i][j] =1;
 				}
-				board[i][j] = tmp;
+				if(map[i][j] == 0) preTomato++;
+				
 			}
 		}
-		bfs(board, visited, q);
-		if(preTomato !=0) System.out.println(-1);
-		if(preTomato ==0) System.out.println(days);
-		
+		bfs(map);
+		if(preTomato != 0) System.out.println(-1);
+		if(preTomato == 0) {
+			if(count ==0) System.out.println(0);
+			if(count !=0) System.out.println(count-1);
+		}
 		
 	}
-	public static void bfs(int[][] board, int[][] visited, Queue<int[]> q) {
-		if(preTomato ==0) {
-			days=0;
-			return;
-		}
+	
+	public static void bfs(int[][] map) {
 		while(!q.isEmpty()) {
-			int[] cur = q.poll();
-			int x = cur[0];
-			int y = cur[1];
+			int[] temp = q.poll();
+			int x = temp[0];
+			int y = temp[1];
 			
 			for(int i=0;i<4;i++) {
 				int nx = x + dx[i];
 				int ny = y + dy[i];
 				
-				if(nx>=0 && nx<N && ny>=0 && ny<M) {
-					if(board[nx][ny]==0 && visited[nx][ny] !=1) {
+				if(nx >=0 && nx<N && ny >=0 && ny<M) {
+					if(map[nx][ny] == 0) {
 						preTomato--;
-						board[nx][ny] = board[x][y] +1;
-						days = Math.max(days, board[nx][ny]-1);
-						visited[nx][ny] = 1;
-						q.add(new int[] {nx, ny});
+						map[nx][ny] = map[x][y]+1;
+						count = Math.max(count, map[nx][ny]);
+						q.add(new int[] {nx,ny});
 					}
 				}
 			}
